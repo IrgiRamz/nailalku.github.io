3fd80f51f45c6e247c247fda7740beb5d0ad921d
IrgiRamz <106417951+IrgiRamz@users.noreply.github.com>
2025-02-15 06:28:44 +0700

    Update main.js

--- script/main.js
+++ script/main.js
@@ -149,7 +149,7 @@ const animationTimeline = () => {
       ".idea-6 span",
       0.8,
       {
-        scale: 2,
+        scale: 1.5,
         opacity: 0,
         rotation: 15,
         ease: Expo.easeOut,
@@ -160,7 +160,7 @@ const animationTimeline = () => {
       ".idea-6 span",
       0.8,
       {
-        scale: 2,
+        scale: 1.5,
         opacity: 0,
         rotation: -15,
         ease: Expo.easeOut,
